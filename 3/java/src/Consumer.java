3f2422497cd19f5f77218a5cba279c27b68e121d
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-05-02 21:58:23 +0300

    java lab3

---
+++ 3/java/src/Consumer.java
@@ -0,0 +1,28 @@
+public class Consumer implements Runnable{
+    public String name;
+    private Storage st;
+    private int needProductQuantity;
+
+    public Consumer(String name, int needProductQuantity, Storage st){
+        this.name = name;
+        this.needProductQuantity = needProductQuantity;
+        this.st = st;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < needProductQuantity; i++) {
+            try {
+                st.notEmpty.acquire();
+                st.access.acquire();
+                Thread.sleep(1000);
+                st.products.remove(0);
+                System.out.println(this.name + " забрав товар. В сховищі " + st.products.size() + " продуктів.");
+                st.notFull.release();
+                st.access.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ 3/java/src/Producer.java
@@ -0,0 +1,28 @@
+public class Producer implements Runnable {
+    public String name;
+    private Storage st;
+    private int needProductQuantity;
+
+    public Producer(String name, int needProductQuantity, Storage st){
+        this.name = name;
+        this.needProductQuantity = needProductQuantity;
+        this.st = st;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < needProductQuantity; i++){
+            try {
+                st.notFull.acquire();
+                st.access.acquire();
+                Thread.sleep(1000);
+                st.products.add("Product");
+                System.out.println(name + " додав товар. В сховищі " + st.products.size() + " продуктів.");
+                st.notEmpty.release();
+                st.access.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
