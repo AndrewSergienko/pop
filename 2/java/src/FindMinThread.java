57a1bdd9ebdf9f251d1226e7c5ae59260ed8137d
AndrewSergienko <sergienkoandrew9922@gmail.com>
2022-05-06 00:08:36 +0300

    java lab2

--- 2/java/src/FindMinThread.java
+++ 2/java/src/FindMinThread.java
@@ -1,23 +1,16 @@
 public class FindMinThread implements Runnable {
     private int startIndex, endIndex;
-    private int[] array;
+    private MinClass minClass;
 
-    public FindMinThread(int startIndex, int endIndex, int[] array){
+    public FindMinThread(int startIndex, int endIndex, MinClass minClass){
         this.startIndex = startIndex;
         this.endIndex = endIndex;
-        this.array = array;
+        this.minClass = minClass;
     }
 
     @Override
     public void run() {
-        int min = Integer.MAX_VALUE;
-        int index = 0;
-        for (int i = startIndex; i < endIndex; i++) {
-            if(array[i] < min){
-                min = array[i];
-                index = i;
-            }
-        }
-        Main.setMin(min, index);
+        int[] min = minClass.partMin(startIndex, endIndex);
+        minClass.setMin(min[0], min[1]);
     }
 }
--- 2/java/src/Main.java
+++ 2/java/src/Main.java
@@ -1,28 +1,18 @@
 import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    static int minValue = Integer.MAX_VALUE;
-    static int minIndex = 0;
     public static void main(String[] args) {
-        Random random = new Random();
-        int arrLen = 1000;
-        int threadsNum = 10;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Величина масиву: ");
+        int arrLen = sc.nextInt();
+        System.out.println("Кількість потоків: ");
+        int threadsNum = sc.nextInt();
 
-        int[] nums = new int[arrLen];
-        Thread[] threads = new Thread[threadsNum];
+        MinClass minClass = new MinClass(arrLen);
 
-        for(int i = 0; i < arrLen; i++){
-            nums[i] = random.nextInt(-100000, 100000);
-        }
-
-        int partLen = arrLen/threadsNum;
-        for(int i = 0; i < threadsNum-1; i++){
-            threads[i] = new Thread(new FindMinThread(i*partLen, (i+1)*partLen, nums));
-            threads[i].start();
-        }
-        threads[threads.length-1] = new Thread(new FindMinThread((threads.length-1)*partLen, arrLen, nums));
-        threads[threads.length-1].start();
+        Thread[] threads = minClass.threadsMin(threadsNum);
 
         // Очікування завершення всих потоків
         boolean notFinished = true;
@@ -35,21 +25,9 @@ public class Main {
                 notFinished = false;
             }
         }
+
         // Перевірка в межах одного потоку
-        int checkMinValue = Integer.MAX_VALUE;
-        int checkMinIndex = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if(nums[i] < checkMinValue){
-                checkMinValue = nums[i];
-                checkMinIndex = i;
-            }
-        }
-        System.out.println("Результат: nums[" + minIndex + "] = " + minValue + "; Перевірка: nums[" + checkMinIndex + "] = " + checkMinValue);
-    }
-    public static synchronized void setMin(int value, int index){
-        if(value < minValue){
-            minValue = value;
-            minIndex = index;
-        }
+        int[] checkMin = minClass.partMin(0, arrLen);
+        System.out.println("Результат: nums[" + minClass.minIndex + "] = " + minClass.minValue + "; Перевірка: nums[" + checkMin[1] + "] = " + checkMin[0]);
     }
 }
